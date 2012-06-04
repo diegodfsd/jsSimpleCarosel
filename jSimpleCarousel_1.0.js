0bffa5c384d0b3e76d8c9cbb31ca48112cfb3f47
Diego Dias <diego.dfsd@gmail.com>
2012-06-04 17:35:57 -0300

    primeiro commit

---
+++ jSimpleCarousel_1.0.js
@@ -0,0 +1,62 @@
+﻿(function($) {
+    $.fn.jSimpleCarousel = function( options ) {
+        settings = $.extend({
+			childType: 'div',
+            btnNext: null,
+            btnPrev: null,
+            speed: 200,
+            displacement: 145,
+            scroll: 1,
+            visible: 0,
+			forever: true
+        }, options || {});
+
+			
+        return this.each(function() {
+            var running = false,
+                curr = 0,
+				deslocamento = settings.displacement,
+				s = settings.scroll,
+				container = $(this),
+				ths = $(settings.childType, container),
+				only = settings.visible > 0 ? Math.max(settings.visible, ths.length) - settings.visible : ths.length,
+				infinite = settings.forever;
+			
+            if (settings.btnNext) {
+                $(settings.btnNext).click(function() {
+                    return go( curr + s );
+                });
+            };
+
+            if ( settings.btnPrev ) {
+                $(settings.btnPrev).click(function() {
+                    return go( curr - s );
+                });
+            };
+
+            function go( to ) {
+                if ( !running ) {
+					if ( !infinite && (to < 0 || to > only) ) return;
+				
+					if( infinite && to < 0 ){
+						to = curr + only;
+					}else if( infinite && to > only ) {
+						to = 0;
+					}
+					
+                    running = true;
+                    curr = to;
+                    container.animate({ left: -(curr * deslocamento) }, 200);					
+                    running = false;
+                }
+                return false;
+            };
+			
+			function init( container ){
+				var carouselContainer = container.wrap($("<div />").attr("id", "simpleCarouselMain").addClass("simpleCarouselMain"));
+			}
+			
+			init( container );
+        });
+    };
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,4 @@
+jsSimpleCarosel
+====================
+
+jQuery plugin to created a carousel with elements of the DOM
